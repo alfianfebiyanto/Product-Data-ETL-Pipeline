8cb5845bc3a81ed8d2e3d509778d06e078f23bbb
alfianfebiyanto <alfianfebiyanto2@gmail.com>
2025-09-07 20:53:46 +0700

    test pipeline

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+# Import Library
 import pandas as pd
-from src.utils import extract_csv,extract_multi_csv,load_csv
 from config.setting import REQ_PATH, PRODUCTS_PATH, OUTPUT_PATH
+from src.transform import transform_data_products,transform_data_req
+from src.utils import extract_csv,extract_multi_csv,load_csv
 
-# Extract
+#extract
 df_req = extract_csv(REQ_PATH)
 df_product = extract_multi_csv(PRODUCTS_PATH)
 
+# Transform
+df_req_final = transform_data_req(df_req)
+df_product_final = transform_data_products(df_product)
 
-
-
+# load
+load_csv(df_req_final,OUTPUT_PATH,"requirement_final.csv")
+load_csv(df_product_final,OUTPUT_PATH,"product_final.csv")
